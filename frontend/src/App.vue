7995b0ccf7207c2edc514b9e26c419d2ac53b692
dearfad <dearfad@gmail.com>
2024-10-21 22:19:20 +0800

    update

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,11 @@
 <template>
-  <!-- test fastapi -->
-  <button @click="getMessage">Get Message</button>
-  <div>{{ message }}</div>
-
-  <!-- <v-app>
+  <v-app>
     <v-main>
       <router-view />
     </v-main>
-  </v-app> -->
+  </v-app>
 </template>
 
 <script setup>
-// test fastapi
-import axios from "axios";
-const message = ref("default");
-function getMessage() {
-  axios.get("/").then((res) => {
-    message.value = res.data;
-  });
-}
+//
 </script>
--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -1,7 +1,17 @@
 <template>
-  <HelloWorld />
+  <!-- <HelloWorld /> -->
+  <!-- test fastapi -->
+  <v-btn prepend-icon="$vuetify" stacked @click="getMessage">读取模型</v-btn>
+  <v-card>{{ message }}</v-card>
 </template>
 
 <script setup>
-  //
+// test fastapi
+import axios from "axios"
+const message = ref("default")
+function getMessage() {
+  axios.get("/").then((res) => {
+    message.value = res.data
+  })
+}
 </script>
