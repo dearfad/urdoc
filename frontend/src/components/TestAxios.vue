75f87aa9d3094b9c897ccb099134eca871150478
dearfad <dearfad@gmail.com>
2024-10-22 21:54:19 +0800

    update

--- frontend/src/components/TestAxios.vue
+++ frontend/src/components/TestAxios.vue
@@ -1,15 +1,7 @@
 <template>
     <!-- test fastapi -->
-    <v-container>
-        <v-row justify="center">
-            <v-btn prepend-icon="$vuetify" stacked @click="getMessage">读取模型</v-btn>
-        </v-row>
-        <v-row justify="center">
-            <v-card title="Test Axios" subtitle="Backend FastAPI">
-                <v-card-text>{{ message }}</v-card-text>
-            </v-card>
-        </v-row>
-    </v-container>
+    <v-btn class="mx-auto" width="100px" height="100px" prepend-icon="$vuetify" stacked @click="getMessage">读取模型</v-btn>
+    <v-card class="mx-auto my-8" elevation="16" width="250px" height="250px" title="Test Axios" subtitle="Backend FastAPI" :text="message.message"></v-card>
 </template>
 
 <script setup>
