56d88e8cef497796ba21770ab34e68f57f26a48b
dearfad <dearfad@gmail.com>
2024-11-19 18:28:41 +0800

    add nuxthub

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['vuetify-nuxt-module', '@nuxt/eslint', '@pinia/nuxt', '@nuxtjs/eslint-module']
+  modules: [
+    'vuetify-nuxt-module',
+    '@nuxt/eslint',
+    '@pinia/nuxt',
+    '@nuxtjs/eslint-module',
+    '@nuxthub/core'
+  ]
 })
