d435bc8820e27315220aacbb95228c3cb097b81b
dearfad <dearfad@gmail.com>
2024-11-20 21:57:50 +0800

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
-  modules: [
-    'vuetify-nuxt-module',
-    '@nuxt/eslint',
-    '@pinia/nuxt',
-    '@nuxtjs/eslint-module',
-    '@nuxthub/core'
-  ]
+    compatibilityDate: '2024-04-03',
+    devtools: { enabled: false },
+    modules: [
+      'vuetify-nuxt-module',
+      '@nuxt/eslint',
+      '@pinia/nuxt',
+      '@nuxtjs/eslint-module',
+      '@nuxthub/core',
+      '@nuxt/content',
+    ],
 })
