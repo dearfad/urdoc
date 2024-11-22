4ada2a0dd9fb93d06d3f3b7f672b6c0040076eb8
dearfad <dearfad@gmail.com>
2024-11-22 15:40:53 +0800

    Update guide.vue

--- pages/guide.vue
+++ pages/guide.vue
@@ -1,46 +1,26 @@
 <template>
-    <v-stepper alt-labels editable>
-        <v-stepper-header>
-            <v-stepper-item title="生成" value="1" />
-            <v-divider />
-            <v-stepper-item title="模拟" value="2" />
-            <v-divider />
-            <v-stepper-item title="编写" value="3" />
-            <v-divider />
-            <v-stepper-item title="设计" value="4" />
-            <v-divider />
-            <v-stepper-item title="评估" value="5" />
-        </v-stepper-header>
-        <v-stepper-window>
-            <v-stepper-window-item value="1">
-                <v-card title="Step One" flat>...</v-card>
-            </v-stepper-window-item>
-            <v-stepper-window-item value="2">
-                <v-card title="Step One" flat>...</v-card>
-            </v-stepper-window-item>
-            <v-stepper-window-item value="3">
-                <v-card title="Step One" flat>...</v-card>
-            </v-stepper-window-item>
-            <v-stepper-window-item value="4">
-                <v-card title="Step One" flat>...</v-card>
-            </v-stepper-window-item>
-            <v-stepper-window-item value="5">
-                <v-card title="Step One" flat>...</v-card>
-            </v-stepper-window-item>
-        </v-stepper-window>
+    <v-stepper
+        prev-text="上一步"
+        next-text="下一步"
+        :items="['生成病例', '模拟问诊', '编写故事', '设计问题', '评估能力']"
+    >
+        <template #item.1>
+            <v-card title="Step 1">...</v-card>
+        </template>
 
-        <!-- <template #item.2>
-            <v-card title="Step Two" flat>...</v-card>
+        <template #item.2>
+            <v-card title="Step 2">...</v-card>
         </template>
 
         <template #item.3>
-            <v-card title="Step Three" flat>...</v-card>
+            <v-card title="Step 3">...</v-card>
         </template>
         <template #item.4>
-            <v-card title="Step 4" flat>...</v-card>
+            <v-card title="Step 4">...</v-card>
         </template>
+
         <template #item.5>
-            <v-card title="Step 5" flat>...</v-card>
-        </template> -->
+            <v-card title="Step 5">...</v-card>
+        </template>
     </v-stepper>
 </template>
