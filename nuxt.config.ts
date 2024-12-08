fee30b5cac44877fbb6da61ce895e4d9f941f9bd
dearfad <dearfad@gmail.com>
2024-12-08 08:54:00 +0800

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,16 @@
 export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
     devtools: { enabled: false },
+    site: {
+        url: 'https://urdoc.dearfad.com',
+        name: 'URDOC',
+    },
+    seo: {
+        meta: {
+            description:
+                'URDOC, Virtual Case Research Institute, is an integrated virtual case study platform that utilizes large language models to generate medical cases, compose stories, design questions, simulate consultations, and assess capabilities.',
+        },
+    },
     modules: [
         'vuetify-nuxt-module',
         '@nuxt/eslint',
@@ -11,6 +21,7 @@ export default defineNuxtConfig({
         '@nuxt/content',
         'nuxt-cloudflare-analytics',
         '@nuxtjs/supabase',
+        '@nuxtjs/seo',
     ],
     cloudflareAnalytics: {
         token: '97db8550f18d4cdda844ff8ab2199b36',
