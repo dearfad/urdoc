9328dee417b9be44ad9f8d32205ea928e97ad606
dearfad <dearfad@gmail.com>
2025-02-18 16:05:18 +0800

    test

--- server/api/supabase.ts
+++ server/api/supabase.ts
@@ -1,12 +1,14 @@
 import { serverSupabaseClient } from '#supabase/server'
 export default defineEventHandler(async (event) => {
-  const supabaseClient = await serverSupabaseClient(event)
-  const { data, error } = await supabaseClient
-    .from('cases')
-    .select(
-      'id, book, chapter, section, subsection, casetag, content, public, validated, platform, model_name, model_id'
-    )
-    .order('id')
+  const runtimeConfig = useRuntimeConfig()
+  return runtimeConfig.public
+  // const supabaseClient = await serverSupabaseClient(event)
+  // const { data, error } = await supabaseClient
+  //   .from('cases')
+  //   .select(
+  //     'id, book, chapter, section, subsection, casetag, content, public, validated, platform, model_name, model_id'
+  //   )
+  //   .order('id')
 
-  return { data, error }
+  // return { data, error }
 })
