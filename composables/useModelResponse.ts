1b61a2634e607c2d30a39bcdb7e46ffbc11bb167
dearfad <dearfad@gmail.com>
2025-03-16 00:20:21 +0800

    Update useModelResponse.ts

--- composables/useModelResponse.ts
+++ composables/useModelResponse.ts
@@ -5,67 +5,67 @@ export default function () {
     stateStore.modelResponseStream = ''
     stateStore.modelResponseString = ''
     stateStore.modelResponseField = ''
+    const lineRegex = /data: (?:\[.*?\]|\{.*?\})/
     let dataFieldPointer = 0
-    //
-    const response = await $fetch('/api/case/create', {
+    let tempIncompleteLine = ''
+
+    // Consuming SSE (Server Sent Events) via POST request
+    // https://nuxt.com/docs/getting-started/data-fetching
+
+    // Make a POST request to the SSE endpoint
+    const response = await $fetch<ReadableStream>('/api/case/create', {
       method: 'POST',
       body: {
         params: params,
       },
       responseType: 'stream',
     })
-    // const response = await $fetch(params.url, {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //     Authorization: 'Bearer ' + params.apiKey,
-    //   },
-    //   body: {
-    //     model: params.model,
-    //     messages: params.messages,
-    //     stream: true,
-    //     temperature: 0.95,
-    //     top_p: 0.7,
-    //     max_tokens: 1024,
-    //     response_format: params.responseFormat,
-    //   },
-    //   responseType: 'stream',
-    // })
-    if (!response) {
-      return (stateStore.appInfo = '获取模型响应数据为空！')
-    }
 
-    // Consuming SSE (Server Sent Events) via POST request
-    // https://nuxt.com/docs/getting-started/data-fetching
-    const reader = (response as ReadableStream).pipeThrough(new TextDecoderStream()).getReader()
+    // Create a new ReadableStream from the response with TextDecoderStream to get the data as text
+    const reader = response.pipeThrough(new TextDecoderStream()).getReader()
     while (true) {
       const { value, done } = await reader.read()
+      // console.log('value: ', value)
+      // console.log('done: ', done)
       if (done) break
+
+      // DEBUG页面 原始信息流
       stateStore.modelResponseStream += value
 
       // 解析模型响应数据流
       const lines = value.split('\n\n')
+
       lines.forEach((line: string) => {
-        if (line != '' && line != 'data: [DONE]') {
-          try {
-            const jsonDataStr = line.split('data: ')[1].trim()
-            const jsonData = JSON.parse(jsonDataStr)
-            // 临时更改，待阿里云修正stream
-            if (params.model === 'deepseek-v3') {
-              stateStore.modelResponseString = jsonData.choices[0].message.content
-            } else {
-              stateStore.modelResponseString += jsonData.choices[0].delta.content
-            }
-            // 更新当前生成字段
-            if (params.watchFields.length > 0) {
-              if (stateStore.modelResponseString.includes(params.watchFields[dataFieldPointer])) {
-                stateStore.modelResponseField = params.watchFields[dataFieldPointer]
-                dataFieldPointer++
-              }
-            }
-          } catch (error) {
-            return (stateStore.appInfo = `解析模型相应数据失败：${error}`)
+        if (line === '') return
+        // fix 单行不完整
+        line = tempIncompleteLine + line
+        if (lineRegex.test(line)) {
+          tempIncompleteLine = ''
+        } else {
+          tempIncompleteLine = line
+          return
+        }
+        if (line === 'data: [DONE]') return
+
+        const jsonDataStr = line.split('data: ')[1]
+        try {
+          const jsonData = JSON.parse(jsonDataStr)
+          // 临时更改，待阿里云修正stream
+          if (params.model === 'deepseek-v3') {
+            stateStore.modelResponseString = jsonData.choices[0].message.content
+          } else {
+            stateStore.modelResponseString += jsonData.choices[0].delta.content
           }
+          // 更新当前生成字段
+          if (params.watchFields.length > 0) {
+            if (stateStore.modelResponseString.includes(params.watchFields[dataFieldPointer])) {
+              stateStore.modelResponseField = params.watchFields[dataFieldPointer]
+              dataFieldPointer++
+            }
+          }
+        } catch (error) {
+          console.log('*******************************************************')
+          stateStore.appInfo = `解析模型数据流失败：${error}`
         }
       })
     }
