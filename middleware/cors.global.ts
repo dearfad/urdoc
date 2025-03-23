14162036ed4763d211e8eac92b785fd55fcc05ff
dearfad <dearfad@gmail.com>
2025-03-23 21:07:43 +0800

    Delete cors.global.ts

--- middleware/cors.global.ts
+++
@@ -1,5 +0,0 @@
-export default defineEventHandler(async (event) => {
-  event.node.res.setHeader('Access-Control-Allow-Origin', '*') // 允许所有来源访问
-  event.node.res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-  event.node.res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-})
