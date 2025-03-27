8682f5f2f0e88d6f2a7eeea4a2322d31bd2fbc5f
dearfad <dearfad@gmail.com>
2025-03-27 18:18:53 +0800

    Revert "update fonts"
    This reverts commit 66493decce56ae7d38009e19407aee5643fa8444.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,16 @@ export default defineNuxtConfig({
         'URDOC Virtual Case Research Institute: Pioneering in AI-generated medical cases, storytelling, question formulation, and consultation simulation for comprehensive capability assessment in healthcare education and research.',
     },
   },
-  modules: ['@nuxt/eslint', 'vuetify-nuxt-module', '@pinia/nuxt', 'pinia-plugin-persistedstate/nuxt', '@nuxthub/core', '@nuxtjs/supabase', '@nuxtjs/seo', '@nuxtjs/mdc', '@nuxt/fonts'],
+  modules: [
+    '@nuxt/eslint',
+    'vuetify-nuxt-module',
+    '@pinia/nuxt',
+    'pinia-plugin-persistedstate/nuxt',
+    '@nuxthub/core',
+    '@nuxtjs/supabase',
+    '@nuxtjs/seo',
+    '@nuxtjs/mdc',
+  ],
   supabase: {
     redirect: false,
   },
