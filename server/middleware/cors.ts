0cd93b49d85f27f1702f526410b72bed778860de
dearfad <dearfad@gmail.com>
2025-03-29 19:45:50 +0800

    Update cors.ts

--- server/middleware/cors.ts
+++ server/middleware/cors.ts
@@ -6,4 +6,9 @@ export default defineEventHandler((event) => {
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   )
+  if (event.method === 'OPTIONS') {
+    event.node.res.statusCode = 204
+    event.node.res.statusMessage = 'No Content.'
+    return 'OK'
+  }
 })
