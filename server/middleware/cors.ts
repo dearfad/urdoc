f488bf604ca81784819cd71b29016be323060872
dearfad <dearfad@gmail.com>
2025-03-29 19:30:04 +0800

    Update cors.ts

--- server/middleware/cors.ts
+++ server/middleware/cors.ts
@@ -1,5 +1,9 @@
 export default defineEventHandler((event) => {
   setHeader(event, 'Access-Control-Allow-Origin', '*')
   setHeader(event, 'Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-  setHeader(event, 'Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  setHeader(
+    event,
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  )
 })
