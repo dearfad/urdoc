771fdf7ea6571ab6b85ea354c038a52cae257015
dearfad <dearfad@gmail.com>
2025-03-29 14:38:55 +0800

    Update Bar.vue

--- components/App/Bar.vue
+++ components/App/Bar.vue
@@ -1,19 +1,18 @@
 <template>
-    <v-app-bar flat density="compact">
-        <v-app-bar-nav-icon
-            @click="stateStore.isNavDrawerLeftShow = !stateStore.isNavDrawerLeftShow"
-        />
-        <v-app-bar-title class="font-weight-bold cursor-pointer" @click="router.push('/')">
-            URDOC
-        </v-app-bar-title>
-        <v-spacer />
-        <template #append>
-            <v-btn
-                icon="mdi-dots-vertical"
-                @click="stateStore.isNavDrawerRightShow = !stateStore.isNavDrawerRightShow"
-            />
-        </template>
-    </v-app-bar>
+  <v-app-bar flat density="compact">
+    <v-app-bar-nav-icon @click="stateStore.isNavDrawerLeftShow = !stateStore.isNavDrawerLeftShow" />
+    <v-app-bar-title class="font-weight-bold cursor-pointer" @click="router.push('/')">
+      URDOC
+    </v-app-bar-title>
+    <v-spacer />
+    <v-btn text="文档" to="/docs" />
+    <template #append>
+      <v-btn
+        icon="mdi-dots-vertical"
+        @click="stateStore.isNavDrawerRightShow = !stateStore.isNavDrawerRightShow"
+      />
+    </template>
+  </v-app-bar>
 </template>
 
 <script setup>
