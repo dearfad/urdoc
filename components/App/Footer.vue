f949121a132c69e1ceb8c8224f1cf8e4ba8b0c65
dearfad <dearfad@gmail.com>
2025-04-01 17:55:03 +0800

    Update Footer.vue

--- components/App/Footer.vue
+++ components/App/Footer.vue
@@ -2,7 +2,7 @@
   <v-footer app>
     <ClientOnly>
       <v-sheet class="mx-auto text-caption">
-        {{ new Date().getFullYear() }} - 虚拟病例研究所 - Ver 20250401 - NuxtHub
+        {{ new Date().getFullYear() }} - 虚拟病例研究所 - Ver 20250401 - Multi
         <span class="font-weight-bold">{{ userStore.user ? userStore.user.name : '访客' }}</span>
       </v-sheet>
     </ClientOnly>
