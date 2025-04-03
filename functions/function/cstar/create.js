49404716d542e7b9f6572a0357ae82f1a255ff8f
dearfad <dearfad@gmail.com>
2025-04-03 17:45:15 +0800

    Update create.js

--- functions/function/cstar/create.js
+++ functions/function/cstar/create.js
@@ -1,6 +1,6 @@
 export async function onRequest({ request }) {
   const json = await request.clone().json()
-  return new Response(json, {
+  return new Response(JSON.stringify(json), {
     headers: {
       'Content-Type': 'application/json; charset=UTF-8',
       'Access-Control-Allow-Origin': '*',
