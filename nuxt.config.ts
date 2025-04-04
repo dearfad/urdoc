76716a1b669a6240859fbaa0679523f67912c1fe
dearfad <dearfad@gmail.com>
2025-04-04 17:21:39 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,9 +15,9 @@ export default defineNuxtConfig({
     'vuetify-nuxt-module',
     '@pinia/nuxt',
     'pinia-plugin-persistedstate/nuxt',
-    '@nuxthub/core',
+    // '@nuxthub/core',
     // 无用，待完善
-    // process.env.HOSTING_PROVIDER === 'nuxthub' ? '@nuxthub/core' : '',
+    process.env.HOSTING_PROVIDER === 'nuxthub' ? '@nuxthub/core' : '',
     '@nuxtjs/supabase',
     '@nuxtjs/seo',
   ],
