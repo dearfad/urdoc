d89559f40938f69eefe390e1db18861332a9f56d
dearfad <dearfad@gmail.com>
2025-04-05 23:48:46 +0800

    Update chat.js

--- functions/function/chat.js
+++ functions/function/chat.js
@@ -12,24 +12,65 @@ export async function onRequest({ request, env }) {
   request.headers.delete('accept-encoding')
   const { params } = await request.clone().json()
   const model = params.models.chatModel
+  const gatewayApiKey = env[model.envGatewayApiKeyName]
   const providerApiKey = env[model.envProviderApiKeyName]
-
-  const stream = await fetch(model.url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: 'Bearer ' + providerApiKey,
-    },
-    body: JSON.stringify({
-      model: model.id,
-      messages: params.messages,
-      response_format: params.responseFormat,
-      stream: true,
-      // temperature: 0.95,
-      // top_p: 0.7,
-      // max_tokens: 1024,
-    }),
-  })
+  let stream = ''
+  switch (model.gateway) {
+    case 'EdgeOne':
+      stream = await fetch(model.url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + providerApiKey,
+          'OE-Gateway-Version': 2,
+          'OE-Key': gatewayApiKey,
+          'OE-Gateway-Name': 'urdoc',
+          'OE-AI-Provider': 'ali',
+        },
+        body: JSON.stringify({
+          model: model.id,
+          messages: params.messages,
+          response_format: params.responseFormat,
+          stream: true,
+          // temperature: 0.95,
+          // top_p: 0.7,
+          // max_tokens: 1024,
+        }),
+      })
+      break
+    case 'OpenRouter':
+      stream = await fetch(model.url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + gatewayApiKey,
+        },
+        body: JSON.stringify({
+          model: model.id,
+          messages: params.messages,
+          response_format: params.responseFormat,
+          stream: true,
+        }),
+      })
+      break
+    default:
+      stream = await fetch(model.url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + providerApiKey,
+        },
+        body: JSON.stringify({
+          model: model.id,
+          messages: params.messages,
+          response_format: params.responseFormat,
+          stream: true,
+          // temperature: 0.95,
+          // top_p: 0.7,
+          // max_tokens: 1024,
+        }),
+      })
+  }
 
   return new Response(stream.body, {
     headers: {
