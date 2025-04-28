ed234ed5d89f463f30325f34f5ebb4840ee04537
dearfad <dearfad@gmail.com>
2025-04-29 07:45:38 +0800

    fix mobile mx-4

--- pages/cstar/story.vue
+++ pages/cstar/story.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-sheet class="d-flex flex-column w-100 w-md-75 mx-auto">
+  <v-sheet class="d-flex flex-column w-100 w-md-75 mx-auto py-2 ga-4 mx-4">
     <CommonRecordTools />
     <StoryContentShow />
     <StoryUtilsGenerate />
