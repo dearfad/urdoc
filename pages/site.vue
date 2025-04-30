371c5db2a76e794e1e57baadf721f7991a5bac22
dearfad <dearfad@gmail.com>
2025-04-30 14:55:06 +0800

    update

--- pages/site.vue
+++ pages/site.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-sheet class="d-flex flex-column w-100 w-md-75 mx-auto mb-16">
+  <v-sheet class="d-flex flex-column w-100 w-md-75 mx-auto py-2 ga-4">
     <CommonSiteSelector />
     <CommonApiBaseUrlSelector />
   </v-sheet>
