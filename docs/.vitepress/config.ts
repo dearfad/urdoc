1528c601d77bdd23922b0e2072489a4dddb206a6
dearfad <dearfad@gmail.com>
2025-05-20 18:03:01 +0800

    add docs learning theory

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -26,6 +26,11 @@ export default withMermaid({
       },
     ],
 
+    docFooter: {
+      prev: '上一页',
+      next: '下一页',
+    },
+
     outline: {
       label: '页面导航',
     },
@@ -40,6 +45,11 @@ export default withMermaid({
     lastUpdated: {
       text: '最后更新于',
     },
+    returnToTopLabel: '回到顶部',
+    sidebarMenuLabel: '菜单',
+    darkModeSwitchLabel: '主题',
+    lightModeSwitchTitle: '切换到浅色模式',
+    darkModeSwitchTitle: '切换到深色模式',
   },
 
   // your existing vitepress config...
