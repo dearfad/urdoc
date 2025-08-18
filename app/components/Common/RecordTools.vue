0fa1302c261b8103169f87cb3d939ee9fc172bd9
dearfad <dearfad@gmail.com>
2025-08-19 07:43:14 +0800

    Disable actions in PromptCard and RecordTools
    Disabled several buttons in PromptCard.vue and RecordTools.vue to prevent user interaction with '刷新', '查看', '编辑', '浏览', '更新', and '保存' actions. Also updated Clerk and Nuxt ESLint dependencies to latest versions for improved compatibility and bug fixes.

--- app/components/Common/RecordTools.vue
+++ app/components/Common/RecordTools.vue
@@ -6,11 +6,12 @@
     :loading="isLoading"
   >
     <v-btn-toggle v-model="stateStore.recordShowContent" mandatory density="compact">
-      <v-btn text="查看" :value="'markdown'" />
-      <v-btn text="编辑" :value="'edit'" />
+      <v-btn disabled text="查看" :value="'markdown'" />
+      <v-btn disabled text="编辑" :value="'edit'" />
     </v-btn-toggle>
     <div class="ml-auto">
       <v-btn
+        disabled
         variant="plain"
         text="浏览"
         class="font-weight-bold"
@@ -18,10 +19,10 @@
       />
     </div>
     <div v-if="recordStore.record.id">
-      <v-btn variant="plain" text="更新" class="font-weight-bold" @click="update()" />
+      <v-btn disabled variant="plain" text="更新" class="font-weight-bold" @click="update()" />
     </div>
     <div v-else>
-      <v-btn variant="plain" text="保存" class="font-weight-bold" @click="insert()" />
+      <v-btn disabled variant="plain" text="保存" class="font-weight-bold" @click="insert()" />
     </div>
   </v-card>
 </template>
