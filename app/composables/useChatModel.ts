36c2acef274d63e9d29c2b83f1d1ddab43fab397
dearfad <dearfad@gmail.com>
2025-09-08 22:34:46 +0800

    migrate edge function

--- app/composables/useChatModel.ts
+++ app/composables/useChatModel.ts
@@ -25,7 +25,7 @@ export default function () {
     // Make a POST request to the SSE endpoint
     let response: ReadableStream
     try {
-      response = await $fetch<ReadableStream>('/model/chat', {
+      response = await $fetch<ReadableStream>('/model/bigmodel/chat', {
         baseURL: stateStore.apiBaseUrl,
         method: 'POST',
         body: {
--- functions/function/model/bigmodel/[[path]].js
+++ functions/function/model/bigmodel/[[path]].js
@@ -1,4 +1,36 @@
+import { unmask } from '../../utils/mask.js'
+const PATH_PREFIX = '/function/model/bigmodel'
 export async function onRequest({ request, env }) {
   const { pathname } = new URL(request.url)
-  return new Response(JSON.stringify(pathname))
+  const route = pathname.slice(PATH_PREFIX.length) || '/'
+
+  if (route.startsWith('/chat')) return chatCompletions(request, env)
+
+  // TODO: error handling
+  return new Response(JSON.stringify(route))
+}
+
+async function chatCompletions(request, env) {
+  request.headers.delete('accept-encoding')
+  const { params } = await request.json()
+  const apiKey = params.apiKey
+    ? unmask(env['NUXT_URDOC_SECRET_KEY'], params.apiKey)
+    : env[params.apiKeyName]
+  params.headers.Authorization = `Bearer ${apiKey}`
+  const response = await fetch(params.url, {
+    method: 'POST',
+    headers: params.headers,
+    body: params.body,
+  })
+  return streamResponse(response)
+}
+
+function streamResponse(response) {
+  return new Response(response.body, {
+    status: response.status,
+    headers: {
+      'Content-Type': response.headers.get('Content-Type') || 'application/octet-stream',
+      'Cache-Control': 'no-store',
+    },
+  })
 }
