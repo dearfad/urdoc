ea387d373095f3180edfc709745cee26af056095
dearfad <dearfad@gmail.com>
2025-10-23 15:08:07 +0800

    Update ScopeSelector.vue

--- app/components/Common/ScopeSelector.vue
+++ app/components/Common/ScopeSelector.vue
@@ -1,129 +1,128 @@
 <template>
-  <v-sheet>
-    <div>
-      <v-btn color="grey-lighten-3" block @click="isExpandShow = !isExpandShow"> 疾病范围 </v-btn>
-      <div class="px-4 py-1 border-dotted border-thin border-t-0">
-        <v-chip-group>
-          <v-chip
-            v-for="item in filteredItems"
-            :key="item"
-            variant="text"
-            :prepend-icon="item.icon"
-          >
-            {{ stateStore.scope[item.name] }}
-          </v-chip>
-        </v-chip-group>
-      </div>
-      <v-expand-transition>
-        <div v-if="isExpandShow">
-          <v-select
-            v-model="book"
-            label="教科书"
-            :items="books"
-            variant="outlined"
-            class="my-4"
-            hide-details="auto"
-            prepend-inner-icon="mdi-book-open-variant-outline"
-            density="comfortable"
-            clearable
-            append-icon="mdi-autorenew"
-            @click:append="randomBook"
-            @update:model-value="handleBookChange"
-          />
-          <v-select
-            v-model="part"
-            label="篇目"
-            :items="parts"
-            variant="outlined"
-            class="my-4"
-            hide-details="auto"
-            density="comfortable"
-            prepend-inner-icon="mdi-bookmark-multiple-outline"
-            clearable
-            :disabled="parts.length === 0"
-            append-icon="mdi-autorenew"
-            @update:model-value="handlePartChange"
-            @click:append="randomPart"
-          />
-          <v-select
-            v-model="chapter"
-            label="章节"
-            :items="chapters"
-            variant="outlined"
-            class="my-4"
-            hide-details="auto"
-            density="comfortable"
-            prepend-inner-icon="mdi-bookmark-multiple-outline"
-            clearable
-            :disabled="chapters.length === 0"
-            append-icon="mdi-autorenew"
-            @click:append="randomChapter"
-            @update:model-value="handleChapterChange"
-          />
-          <v-select
-            v-model="section"
-            label="节次"
-            :items="sections"
-            variant="outlined"
-            class="my-4"
-            hide-details="auto"
-            density="comfortable"
-            prepend-inner-icon="mdi-book-outline"
-            clearable
-            :disabled="sections.length === 0"
-            append-icon="mdi-autorenew"
-            @update:model-value="handleSectionChange"
-            @click:append="randomSection"
-          />
-          <v-select
-            v-model="subsection"
-            label="子节"
-            :items="subsections"
-            variant="outlined"
-            class="my-4"
-            density="comfortable"
-            prepend-inner-icon="mdi-bookmark-outline"
-            clearable
-            :disabled="subsections.length === 0"
-            hide-details="auto"
-            append-icon="mdi-autorenew"
-            @update:model-value="handleSubsectionChange"
-            @click:append="randomSubsection"
-          />
-          <v-select
-            v-model="topic"
-            label="主题"
-            :items="topics"
-            variant="outlined"
-            class="my-4"
-            density="comfortable"
-            prepend-inner-icon="mdi-bookmark-outline"
-            clearable
-            :disabled="topics.length === 0"
-            hide-details="auto"
-            append-icon="mdi-autorenew"
-            @update:model-value="handleTopicChange"
-            @click:append="randomTopic"
-          />
-          <div class="d-flex flex-column align-end mt-2">
-            <v-checkbox
-              v-model="random"
-              max-width="70px"
-              label="随机"
-              density="compact"
-              hide-details
-            />
-          </div>
-        </div>
-      </v-expand-transition>
-    </div>
-  </v-sheet>
+  <v-card subtitle="疾病范围" hover rounded="lg">
+    <v-card-text>
+      <v-chip-group column>
+        <v-chip v-for="item in filteredItems" :key="item" variant="text" :prepend-icon="item.icon">
+          {{ stateStore.scope[item.name] }}
+        </v-chip>
+      </v-chip-group>
+    </v-card-text>
+    <v-expansion-panels v-model="panel" color="grey-lighten-3">
+      <v-expansion-panel value="book" title="请选择">
+        <v-expansion-panel-text>
+          <v-expand-transition>
+            <div v-if="isExpandShow">
+              <v-select
+                v-model="book"
+                label="教科书"
+                :items="books"
+                variant="outlined"
+                class="my-4"
+                hide-details="auto"
+                prepend-inner-icon="mdi-book-open-variant-outline"
+                density="comfortable"
+                clearable
+                append-icon="mdi-autorenew"
+                @click:append="randomBook"
+                @update:model-value="handleBookChange"
+              />
+              <v-select
+                v-model="part"
+                label="篇目"
+                :items="parts"
+                variant="outlined"
+                class="my-4"
+                hide-details="auto"
+                density="comfortable"
+                prepend-inner-icon="mdi-bookmark-multiple-outline"
+                clearable
+                :disabled="parts.length === 0"
+                append-icon="mdi-autorenew"
+                @update:model-value="handlePartChange"
+                @click:append="randomPart"
+              />
+              <v-select
+                v-model="chapter"
+                label="章节"
+                :items="chapters"
+                variant="outlined"
+                class="my-4"
+                hide-details="auto"
+                density="comfortable"
+                prepend-inner-icon="mdi-bookmark-multiple-outline"
+                clearable
+                :disabled="chapters.length === 0"
+                append-icon="mdi-autorenew"
+                @click:append="randomChapter"
+                @update:model-value="handleChapterChange"
+              />
+              <v-select
+                v-model="section"
+                label="节次"
+                :items="sections"
+                variant="outlined"
+                class="my-4"
+                hide-details="auto"
+                density="comfortable"
+                prepend-inner-icon="mdi-book-outline"
+                clearable
+                :disabled="sections.length === 0"
+                append-icon="mdi-autorenew"
+                @update:model-value="handleSectionChange"
+                @click:append="randomSection"
+              />
+              <v-select
+                v-model="subsection"
+                label="子节"
+                :items="subsections"
+                variant="outlined"
+                class="my-4"
+                density="comfortable"
+                prepend-inner-icon="mdi-bookmark-outline"
+                clearable
+                :disabled="subsections.length === 0"
+                hide-details="auto"
+                append-icon="mdi-autorenew"
+                @update:model-value="handleSubsectionChange"
+                @click:append="randomSubsection"
+              />
+              <v-select
+                v-model="topic"
+                label="主题"
+                :items="topics"
+                variant="outlined"
+                class="my-4"
+                density="comfortable"
+                prepend-inner-icon="mdi-bookmark-outline"
+                clearable
+                :disabled="topics.length === 0"
+                hide-details="auto"
+                append-icon="mdi-autorenew"
+                @update:model-value="handleTopicChange"
+                @click:append="randomTopic"
+              />
+              <div class="d-flex flex-column align-end mt-2">
+                <v-checkbox
+                  v-model="random"
+                  max-width="70px"
+                  label="随机"
+                  density="compact"
+                  hide-details
+                />
+              </div>
+            </div>
+          </v-expand-transition>
+        </v-expansion-panel-text>
+      </v-expansion-panel>
+    </v-expansion-panels>
+  </v-card>
 </template>
 
 <script setup>
 const stateStore = useStateStore()
 const bookStore = useBookStore()
 const isExpandShow = ref(true)
+const panel = ref('book')
 const items = ref([
   { icon: 'mdi-book-open-variant-outline', name: 'book' },
   { icon: 'mdi-bookmark-multiple-outline', name: 'part' },
