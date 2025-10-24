1c3afcb15889186b22695d6a5d261a2ff0fbb711
dearfad <dearfad@gmail.com>
2025-10-24 21:03:06 +0800

    Update Illustration.vue

--- app/components/Story/Content/Illustration.vue
+++ app/components/Story/Content/Illustration.vue
@@ -1,26 +1,22 @@
 <template>
-  <v-sheet :height="height">
-    <v-img
-      v-for="(illustration, index) in recordStore.record.story.插图"
-      :key="index"
-      :src="illustration"
-      class="mx-auto my-4"
-      rounded="xl"
-      @click="openIllustrationWindow(illustration)"
-    >
-      <template #placeholder
-        ><div v-if="illustration" class="d-flex align-center justify-center fill-height">
-          <v-progress-circular color="grey-lighten-4" indeterminate /></div
-      ></template>
-    </v-img>
-  </v-sheet>
+  <v-img
+    v-for="(illustration, index) in recordStore.record.story.插图"
+    :key="index"
+    :src="illustration"
+    class="mx-auto my-4"
+    rounded="xl"
+    @click="openIllustrationWindow(illustration)"
+  >
+    <template #placeholder
+      ><div v-if="illustration" class="d-flex align-center justify-center fill-height">
+        <v-progress-circular color="grey-lighten-4" indeterminate /></div
+    ></template>
+  </v-img>
 </template>
 
 <script setup>
 const recordStore = useRecordStore()
-const { height } = defineProps({
-  height: { type: String, default: '50vh', required: false },
-})
+
 function openIllustrationWindow(illustration) {
   // 点击下载，待定
   // window.open(illustration, '_blank')
