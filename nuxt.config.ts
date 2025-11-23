0c1289e453aec8ad4e238afd6a806fad0a7f5e1d
dearfad <dearfad@gmail.com>
2025-11-23 12:30:45 +0800

    move to build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: true,
-  compatibilityDate: '2025-08-08',
+  compatibilityDate: '2025-11-23',
   css: ['~/assets/css/default.css'],
 
   modules: [
