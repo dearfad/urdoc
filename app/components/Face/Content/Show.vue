4c64ce13839ca14fff14aa7eabd5568052bd6889
dearfad <dearfad@gmail.com>
2025-12-15 16:14:41 +0800

    Update Show.vue

--- app/components/Face/Content/Show.vue
+++ app/components/Face/Content/Show.vue
@@ -1,12 +1,5 @@
 <template>
-  <div>
-    <v-img :src="imgUrl" class="mx-auto" rounded="xl" @click="openFaceWindow" />
-    <v-img
-      src="https://render.edgeoneimg.site/52776082fcf452837b825bd68dab155b/3c2cc1f79c0f47bb8654e1e2c70fae80/ep-23cXnH5UMpMz.jpeg"
-      class="mx-auto"
-      rounded="xl"
-    />
-  </div>
+  <v-img :src="imgUrl" class="mx-auto" rounded="xl" @click="openFaceWindow" />
 </template>
 
 <script setup>
@@ -19,22 +12,5 @@ const imgUrl = computed(() => {
 async function openFaceWindow() {
   // 点击下载，待处理点击开新窗口显示图片
   // window.open(recordStore.record.face, '_blank')
-
-  // test 图片渲染
-  const response = await fetch('https://render.edgeoneimg.site', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'OE-USER-ID': '3c2cc1f79c0f47bb8654e1e2c70fae80',
-      'OE-API-KEY': '4QrzDhxuxZzH',
-      'OE-TEMPLATE-ID': 'ep-23cXnH5UMpMz',
-    },
-    body: JSON.stringify({
-      siteName: 'URDOC 虚拟病例研究平台',
-      siteUrl: 'https://dev.urdoc.dearfad.com',
-      // faceUrl: recordStore.record.face,
-    }),
-  })
-  console.log(response)
 }
 </script>
