c013d6c389fb41e5e7ef54d96fc064d838387c1c
dearfad <dearfad@gmail.com>
2025-12-18 17:31:15 +0800

    fix base64

--- edge-functions/function/utils/fetchImageToBase64.js
+++ edge-functions/function/utils/fetchImageToBase64.js
@@ -1,8 +1,9 @@
 export async function onRequest({ request }) {
   const { imgUrl } = await request.json()
   const result = await fetch(imgUrl)
-  const buffer = await result.arrayBuffer()
-  const base64 =
-    `data:${result.headers.get('content-type')};base64,` + Buffer.from(buffer).toString('base64')
+  const array = await result.arrayBuffer()
+  const bytes = new Uint8Array(array)
+  const binary = bytes.reduce((acc, byte) => acc + String.fromCharCode(byte), '')
+  const base64 = `data:${result.headers.get('content-type')};base64,${btoa(binary)}`
   return new Response(base64)
 }
