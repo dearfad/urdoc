c2ef3a8fc4b76e3ded33776a2d4069762358e6d0
dearfad <dearfad@gmail.com>
2025-12-29 18:13:44 +0800

    fix audioButton

--- app/components/Common/AudioButton.vue
+++ app/components/Common/AudioButton.vue
@@ -1,24 +1,54 @@
 <template>
   <div>
-    <v-btn :icon="mdiPlayCircleOutline" :loading="isLoading" @click="play" />
-    <audio v-if="recordStore.record.dialogue" autoplay controls class="w-100 my-2">
-      <source :src="recordStore.record.dialogue" type="audio/mpeg" />
-    </audio>
+    <v-btn :icon="btnIcon" :loading="isLoading" @click="play" />
+    <audio ref="audioPlayer" :src="audioUrl" @play="onPlay" @pause="onPause" @ended="onEnded" />
   </div>
 </template>
 
 <script setup>
-import { mdiPlayCircleOutline } from '@mdi/js'
-const recordStore = useRecordStore()
-const isLoading = ref(false)
+import { mdiPlayCircleOutline, mdiVolumeHigh, mdiPause } from '@mdi/js'
 const { content } = defineProps({
   content: { type: String, required: true },
 })
+const recordStore = useRecordStore()
 
+const audioPlayer = ref(null)
+const audioUrl = ref('')
+const link = 'https://gitee.com/gitee-ai/moark-assets/raw/master/jay_prompt.wav'
+const isLoading = ref(false)
+const isPlaying = ref(false)
+
+const btnIcon = computed(() => {
+  return audioUrl.value ? (isPlaying.value ? mdiPause : mdiPlayCircleOutline) : mdiVolumeHigh
+})
+async function getAudio() {
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+  audioUrl.value = link
+}
 // 截图
 async function play() {
   isLoading.value = true
-  await recordStore.getAudio(content)
+  if (!audioPlayer.value) return
+  if (!audioUrl.value) {
+    await getAudio()
+    isLoading.value = false
+    return
+  }
+  if (audioPlayer.value.paused) {
+    audioPlayer.value.play()
+  } else {
+    audioPlayer.value.pause()
+  }
   isLoading.value = false
+}
+
+function onPlay() {
+  isPlaying.value = true
+}
+function onPause() {
+  isPlaying.value = false
+}
+function onEnded() {
+  isPlaying.value = false
 }
 </script>
