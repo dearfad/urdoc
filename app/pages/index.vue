a0731c941f921c6aefad1def40ddf50593d5df9c
dearfad <dearfad@gmail.com>
2026-03-20 20:15:07 +0800

    update

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,10 +1,11 @@
 <template>
   <UPageHero
-    title="虚拟病例研究平台"
     description="通过大语言模型生成病例、编写故事、设计问题、模拟问诊、评估能力，CSTAR模式的应用与创新实践"
     :links="links"
     headline="URDOC 🌱 CSTAR"
-  />
+  >
+    <template #title><span class="text-4xl sm:text-7xl">虚拟病例研究平台</span></template>
+  </UPageHero>
 </template>
 
 <script setup lang="ts">
