88475e32121ac245b56340f2c67ddea3f0dbd48b
dearfad <dearfad@gmail.com>
2026-05-03 12:53:04 +0800

    Update narrative-medicine.vue

--- app/pages/project/narrative-medicine.vue
+++ app/pages/project/narrative-medicine.vue
@@ -39,7 +39,7 @@
       <UDashboardToolbar />
     </template>
     <template #body>
-      <div class="flex flex-row gap-4">
+      <div class="flex flex-row flex-wrap gap-4">
         <Case />
         <Story />
       </div>
